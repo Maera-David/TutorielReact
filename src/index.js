4832b99f4e03fc6a7485994594bf64a9fb289a1c
Maera <maera.david68@gmail.com>
2020-10-12 12:05:02 +0200

    Add Circle player

--- src/index.js
+++ src/index.js
@@ -2,30 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-    };
-  }
-
-  renderSquare(i) {
-    return <Square value={i} />; 
-  }
-  
-    render() {
+function Square(props) {
       return (
-        <button 
-          className="square" 
-          onClick={() => this.setState({value: 'X'})}>
-          {this.state.value}
+        <button className="square" onClick={props.onClick}>
+          {props.value}
         </button>
       );
     }
-  }
   
   class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+        xIsNext: true,
+      };
+    }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
+    }
+
     renderSquare(i) {
       return (
         <Square
@@ -36,7 +39,7 @@ class Square extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: X' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
